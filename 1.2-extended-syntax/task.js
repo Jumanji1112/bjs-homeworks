9da9f481108810edd3cbfb508fe771ffecc299eb
Alisa A <Jumanji1112@mail.ru>
2021-03-06 19:15:30 +0200

    Исправленна формула дискриминанта. Оптимизация кода. Вид кода

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,45 +3,37 @@
 function getResult(a,b,c){
   let x = [];
   let d;
-    d = b** - 4 * a * c; //дискриминант
-    if (d < 0){
-      return x;
-    } 
-    else if (d == 0){ // массив с 1 корнем
+  d = Math.pow(b, 2) - 4 * a * c; //дискриминант
+    if (d == 0){ // массив с 1 корнем
       x.push ((-b + Math.sqrt(d)) / (2 * a));
-          return x;
     } 
     else if (d > 0){ //массив с 2 корнями
       x.push((-b - Math.sqrt(d)) / (2 * a));
       x.push((-b + Math.sqrt(d)) / (2 * a));
-          return x;
     }
+    console.log(d)
+    return x;
 }
 
 function getAverageMark(marks){
   let sum = 0;
   let amt = (marks.slice (0, 5)); //берем из массива первые 5 
-
-	    for (let i = 0; i < amt.length; i++){
-    	sum += amt[i]; //суммируем оценки
-      }
-	let averageMark = sum / amt.length; // средняя оценка
-    if (marks.length == 0){
-        console.log ('Нет оценок');
-      return  averageMark = 0;
-  } else if (marks.length > 5){
-    console.log ('Количсетво оценок больше 5');
+  for (let i = 0; i < amt.length; i++){
+    sum += amt[i]; //суммируем оценки
   }
-    return averageMark;
+  let averageMark = sum / amt.length; // средняя оценка
+  if (marks.length == 0){
+    return 0;
+  }
+  return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-	let now = new Date; 
-    let age = now.getFullYear() - dateOfBirthday.getFullYear(); //возраст пользователя
-    if (age >= 18){
+  let now = new Date().getFullYear();
+    if (now - dateOfBirthday.getFullYear() >= 18){
     	return(`Не желаете ли олд-фэшн, ${name}?`);
-    } else if (age < 18){
+    }
+    else {
     	return(`Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`);
     }
-     return age;
 }
