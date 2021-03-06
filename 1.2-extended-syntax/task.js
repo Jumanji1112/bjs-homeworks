0a5cc893383cb41ca33d4e977b0827bd6a9f5101
Alisa A <Jumanji1112@mail.ru>
2021-03-06 12:41:37 +0200

    домашнее задание 2. три задачи

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,42 @@
+'use strict'
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+  let x = [];
+  let d;
+    d = b** - 4 * a * c; //дискриминант
+    if (d < 0){
+    } else if (d == 0){ // массив с 1 корнем
+      x = -b / (2 * a); 
+    } else if (d > 0){ //массив с 2 корнями
+      x.push((-b - Math.sqrt(d)) / (2 * a));
+      x.push((-b + Math.sqrt(d)) / (2 * a));
+    }
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+  let sum = 0;
+  let amt = (marks.slice (0, 5)); //берем из массива первые 5 
+	if (marks.length == 0){
+    		console.log ('Нет оценок');
+ 		    averageMark = 0;
+	} else if (marks.length > 5){
+		console.log ('Количсетво оценок больше 5');
+	}
+    for (let i = 0; i < amt.length; i++){
+    	sum += amt[i]; //суммируем оценки
+    }
+	let averageMark = sum / amt.length; // средняя оценка
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+	let now = new Date; 
+    let age = now.getFullYear() - dateOfBirthday.getFullYear(); //возраст пользователя
+    if (age >= 18){
+    	return(`Не желаете ли олд-фэшн, ${name}?`);
+    } else if (age < 18){
+    	return(`Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`);
+    }
+     return age;
 }
