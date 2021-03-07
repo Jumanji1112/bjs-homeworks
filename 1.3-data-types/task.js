93fc369ae028f5e4f15fcbcf58af264c8ed4896f
Alisa A <Jumanji1112@mail.ru>
2021-03-07 19:33:34 +0200

    Решение задач к 3 лекции

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,21 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+	let debet = amount - contribution; //тело кредита
+	let months = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()); //количество месяцев
+	let p = percent / 100 / 12; // процентаная ставка за месяц
+	let monthlyPay =  debet * (p + p / (Math.pow((1+p), months) - 1)); // ежемесячный платеж по кредиту
+	let totalPay = monthlyPay * months; // общая сумма выплат
+		totalPay = totalPay.toFixed(2); // округление  суммы выплат
+	let num = Number(totalPay)
+	  console.log (typeof num);
+    return num;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	if (name == "" || name == undefined){
+		return ('Привет, мир! Меня зовут Аноним.');
+	} else {
+		return (`Привет, мир! Меня зовут ${name}.`);
+	}
 }
