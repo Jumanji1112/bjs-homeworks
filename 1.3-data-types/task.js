7c71626d99b750788e4ab699cd1e5618e5353b21
Alisa A <Jumanji1112@mail.ru>
2021-03-15 12:26:51 +0200

    задача1: внесенa проверка корктности ввода в поля

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,8 +1,18 @@
 'use strict'
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+	percent = +percent;
+	if ( percent == 0 || percent == "" || percent == undefined || typeof percent !== 'number' || percent != percent )		return "Введите корректные данные в поле  \"Процентная ставка\"" ;
+	contribution = +contribution;
+	if (contribution < 0 || contribution == "" || contribution == undefined || typeof contribution !== 'number' || contribution != contribution) return "Введите корректные данные в поле  \"Начальный взнос\"" ;
+	amount = +amount;
+	if (amount <= 0 || amount == "" || amount == undefined || typeof amount !== 'number' || amount != amount) return "Введите корректные данные в поле  \"Общая стоимость\"" ;
+
+
 	let debet = amount - contribution; //тело кредита
 	let months = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()); //количество месяцев
+	if (!date.getDate()) return 'Укажите дату погашения кредита';
+	if (months <= 0) return "Срок ипотеки не может быть менее 1 месеца";
 	let p = percent / 100 / 12; // процентаная ставка за месяц
 	let monthlyPay =  debet * (p + p / (Math.pow((1+p), months) - 1)); // ежемесячный платеж по кредиту
 	let totalPay = monthlyPay * months; // общая сумма выплат
@@ -15,7 +25,6 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 function getGreeting(name) {
 	if (name == "" || name == undefined){
 		return ('Привет, мир! Меня зовут Аноним.');
-	} else {
+	};
 		return (`Привет, мир! Меня зовут ${name}.`);
-	}
-}
+};
