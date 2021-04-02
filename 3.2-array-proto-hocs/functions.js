3854169d52f68c036ecff76ba676982a99bca0cf
Alisa A <Jumanji1112@mail.ru>
2021-04-02 16:31:22 +0300

    Задача1. Задача2

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -28,3 +28,45 @@ return	sumAttack.reduce (function(previousValue, item, index, array){
 	return previousValue + item;
 });
 }
+
+//Задача №2. Ускорение долгих вычислений
+function sleep(milliseconds) 
+{
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {}
+}
+
+function sum(...args) {
+  // Замедление на половину секунды.
+  sleep(100); // Можно использовать другое значение замедления.
+  return args.reduce((sum, arg) => {
+    return sum += +arg;
+  }, 0);
+}
+
+function compareArrays( arr1, arr2 ){
+return  (arr1.length == arr2.length) && arr1.every ((item, index) => item == arr2[index]);
+}
+
+function memorize(fn, limit){
+	let memory = [];
+	return function (...args){
+		let memoryItem = memory.find(item => compareArrays(args, item.args));
+		if (memoryItem) {
+         return memoryItem.result;
+      }
+      let result = fn(...args);
+      memory.push({
+         args: args,
+         result: result
+      });
+      if (memory.length > limit) {
+         memory.shift();
+      }
+      return result;
+	}
+}
+
+const mSum = memorize(sum, 5); 
+sum(3, 4); 
+mSum(3, 4);
